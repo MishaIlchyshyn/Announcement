c393e75bab48626cf3965dc29ce4ffb04cc122a0
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-08-06 18:45:34 +0300

    Add _lodash, method _intersection in similar function

--- src/features/similar.js
+++ src/features/similar.js
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 export function similar(announcements, detailAnnouncement, max) {
   let filterAnn = announcements.filter((item) =>
     isAnnouncementsSimilar(item, detailAnnouncement)
@@ -6,26 +8,13 @@ export function similar(announcements, detailAnnouncement, max) {
   return filterAnn.slice(0, max);
 }
 
-Set.prototype.intersection = function (setB) {
-  var intersection = new Set();
-  for (var elem of setB) {
-    if (this.has(elem)) {
-      intersection.add(elem);
-    }
-  }
-  return intersection;
-};
-
 function isStringsSimilar(str1, str2) {
   let str1Arr = str1.split(" ");
   let str2Arr = str2.split(" ");
 
-  let set1 = new Set(str1Arr);
-  let set2 = new Set(str2Arr);
+  let intersection = _.intersection(str1Arr, str2Arr);
 
-  let intersection = set1.intersection(set2);
-
-  return intersection.size !== 0;
+  return intersection.length !== 0;
 }
 
 function isAnnouncementsSimilar(ann1, ann2) {
